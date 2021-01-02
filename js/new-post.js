dfda50386aadbbafe1ed9b0a0baffaa5e79de7b0
nkoubamarius <nkoubamarius@gmail.com>
2021-01-02 15:28:52 +0100

    Receive form data and send it through API to save it

--- js/new-post.js
+++ js/new-post.js
@@ -6,5 +6,20 @@ const API_URL = "http://localhost:3000/api/posts";
 
 const submitNewPost = () => {
     // HINT: Use FormData to store data to send over
+    const title=document.getElementById('form-post-title').value;
+    const content=document.getElementById('form-post-content').value;
+    const input=document.getElementById('form-post-image');
+
+    let data= new FormData();
+    data.append("post-image", input.files[0]);
+    data.append("title", title);
+    data.append("content", content);
+
+    fetch(API_URL,{ method: "POST", body: data}).then(()=>{
+       setTimeout(()=>{
+        window.location.href="index.html";
+       }, 1000);
+    })
+
     // HINT: Redirect the user to home page after successful submission
 }
