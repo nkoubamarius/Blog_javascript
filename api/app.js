14b6d20a5abe711f71dc6df740de494228ffa5b4
nkoubamarius <nkoubamarius@gmail.com>
2021-01-02 14:29:58 +0100

    Adding multer configuration to app.js

--- api/app.js
+++ api/app.js
@@ -1,10 +1,24 @@
 const express=require("express");
 const Post=require("./models/posts");
 var cors = require('cors');
+var multer=require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${file.fieldname}-${Date.now()}.jpg`)
+    }
+});
+   
+var upload = multer({ storage: storage });
+
 const postData=new Post();
 const app=express();
 
 // app.use(cors());
+
+app.use(express.json());
 
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -27,5 +41,18 @@ app.get("/api/posts/:post_id",(req, res)=>{
     }
 
 });
+
+app.post('/api/posts',upload.single("post-image"),(req, res)=>{
+    const newPost={
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image":req.body["post-image"],
+        "added-date":`${Date.now()}`
+    }
+
+    postData.add(newPost);
+    res.status(201).send(newPost);
+})
 
 app.listen(3000, ()=>console.log("listening on http://localhost:3000"));
